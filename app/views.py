321f7b49f2c26c506bdd7ace658632a6f0d77b91
imwillson <liwillson1@gmail.com>
2017-07-16 22:40:58 -0400

    add HTML template

--- app/views.py
+++ app/views.py
@@ -1,7 +1,27 @@
+from flask import render_template
 from app import app 
 
 # view function wrapped to this route 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'Willson'}
+	posts = [
+		{ 
+		'author': {'nickname': 'John'}, 
+		'body': 'Beautiful day in Portland!' 
+		},
+		{ 
+		'author': {'nickname': 'Susan'}, 
+		'body': 'The Avengers movie was so cool!' 
+		}
+
+	]
+	return render_template('index.html', 
+							title='Home',
+							user=user, 
+							posts=posts) 
+	# these are positional arguments, passed in from 
+	# the funciont
+
+
